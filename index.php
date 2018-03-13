c42797bb0ce2059466efdaa2bcbebc89cd5ac148
Freddy Nygård <freddnyg@FreddysMacBookPro.local>
2018-03-13 07:41:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'php-includes/header.php'; ?>
+<?php include 'php-includes/nav.php'; ?>
+
+	  <div class="container">
+		  <h1>Eleah Adine</h1>
+		  <img src="media/image.jpg" alt="image" />
+	  </div>
+	  
+<?php include 'php-includes/footer.php'; ?>
